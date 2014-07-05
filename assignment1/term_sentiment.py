01f0ad703d1d262e583b51fddce447a6684156c9
Luqi Pan <luqi.pan@gmail.com>
2014-07-04 21:48:10 -0400

    Added a dict for 2-word keys for problem 3

--- assignment1/term_sentiment.py
+++ assignment1/term_sentiment.py
@@ -1,17 +1,31 @@
 import sys
 
-def hw():
-    print 'Hello, world!'
-
-def lines(fp):
-    print str(len(fp.readlines()))
-
 def main():
     sent_file = open(sys.argv[1])
     tweet_file = open(sys.argv[2])
-    hw()
-    lines(sent_file)
-    lines(tweet_file)
+
+    one_word_scores = {}
+    two_word_scores = {}
+
+    for line in sent_file:
+        term, score = line.split("\t")
+        one_word_scores[term] = int(score)
+
+    for line in sent_file:
+        term, score = line.split("\t")
+        if ' ' in term:
+            one_word_scores.pop(term, None)
+            two_word_scores[term] = int(score)
+
+    for line in tweet_file:
+        tweet = json.loads(line)
+        if (tweet.has_key("text")):
+            text = tweet["text"]
+            terms = text.split(" ")
+            sum = 0
+            for term in terms:
+                sum += scores.get(term, 0)
+            print sum
 
 if __name__ == '__main__':
     main()
