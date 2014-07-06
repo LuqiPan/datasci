8c3b92eda643788f531a81378b8ddd66c3ec16ee
Luqi Pan <luqi.pan@gmail.com>
2014-07-05 22:04:58 -0400

    Completed problem 3.
    Developed LuqiScore

--- assignment1/term_sentiment.py
+++ assignment1/term_sentiment.py
@@ -1,4 +1,5 @@
 import sys
+import json
 
 def main():
     sent_file = open(sys.argv[1])
@@ -6,6 +7,8 @@ def main():
 
     one_word_scores = {}
     two_word_scores = {}
+
+    luqi_scores = {}
 
     for line in sent_file:
         term, score = line.split("\t")
@@ -22,10 +25,35 @@ def main():
         if (tweet.has_key("text")):
             text = tweet["text"]
             terms = text.split(" ")
-            sum = 0
+            span = 2
+            sum, abs_sum = [0, 0]
+
+            for i in range(0, len(terms), span):
+                score = two_word_scores.get(" ".join(terms[i:i+span]), 0)
+                sum += score
+                abs_sum += abs(score)
+
             for term in terms:
-                sum += scores.get(term, 0)
-            print sum
+                score = one_word_scores.get(term, 0)
+                sum += score
+                abs_sum += abs(score)
+
+            if abs_sum == 0:
+                luqi_score = 0
+            else:
+                luqi_score = float(sum) / abs_sum * 10
+
+            for term in terms:
+                if not one_word_scores.has_key(term):
+                    if '@' in term:
+                        continue
+                    if luqi_scores.has_key(term):
+                        luqi_scores[term] += luqi_score
+                    else:
+                        luqi_scores[term] = luqi_score
+
+    for k, v in luqi_scores.iteritems():
+        print k + " " + str(v)
 
 if __name__ == '__main__':
     main()
