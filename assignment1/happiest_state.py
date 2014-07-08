0e6038630945d73d58f7124b41fa532e79d7d8d3
Luqi Pan <luqi.pan@gmail.com>
2014-07-07 23:28:32 -0400

    Got the basic data and routine in place
    Needed to develop more logic to solve this problem

---
+++ assignment1/happiest_state.py
@@ -0,0 +1,101 @@
+import sys
+import json
+
+states = {
+    'AK': 'Alaska',
+    'AL': 'Alabama',
+    'AR': 'Arkansas',
+    'AS': 'American Samoa',
+    'AZ': 'Arizona',
+    'CA': 'California',
+    'CO': 'Colorado',
+    'CT': 'Connecticut',
+    'DC': 'District of Columbia',
+    'DE': 'Delaware',
+    'FL': 'Florida',
+    'GA': 'Georgia',
+    'GU': 'Guam',
+    'HI': 'Hawaii',
+    'IA': 'Iowa',
+    'ID': 'Idaho',
+    'IL': 'Illinois',
+    'IN': 'Indiana',
+    'KS': 'Kansas',
+    'KY': 'Kentucky',
+    'LA': 'Louisiana',
+    'MA': 'Massachusetts',
+    'MD': 'Maryland',
+    'ME': 'Maine',
+    'MI': 'Michigan',
+    'MN': 'Minnesota',
+    'MO': 'Missouri',
+    'MP': 'Northern Mariana Islands',
+    'MS': 'Mississippi',
+    'MT': 'Montana',
+    'NA': 'National',
+    'NC': 'North Carolina',
+    'ND': 'North Dakota',
+    'NE': 'Nebraska',
+    'NH': 'New Hampshire',
+    'NJ': 'New Jersey',
+    'NM': 'New Mexico',
+    'NV': 'Nevada',
+    'NY': 'New York',
+    'OH': 'Ohio',
+    'OK': 'Oklahoma',
+    'OR': 'Oregon',
+    'PA': 'Pennsylvania',
+    'PR': 'Puerto Rico',
+    'RI': 'Rhode Island',
+    'SC': 'South Carolina',
+    'SD': 'South Dakota',
+    'TN': 'Tennessee',
+    'TX': 'Texas',
+    'UT': 'Utah',
+    'VA': 'Virginia',
+    'VI': 'Virgin Islands',
+    'VT': 'Vermont',
+    'WA': 'Washington',
+    'WI': 'Wisconsin',
+    'WV': 'West Virginia',
+    'WY': 'Wyoming'
+}
+
+def main():
+    def get_tweet_score(tweet):
+        if (tweet.has_key("text")):
+            text = tweet["text"]
+            terms = text.split(" ")
+            sum = 0
+            for term in terms:
+                sum += scores.get(term, 0)
+            return sum
+        else:
+            return 0
+
+    def get_location_words(tweet):
+        if (tweet.has_key("place")):
+            if tweet["place"]:
+                if "," in tweet["place"]["full_name"]:
+                    words = tweet["place"]["full_name"].split(",")
+                    word1, word2 = [words[0], words[1]]
+                    word2 = word2[1:]
+                    return [word1, word2]
+        return [None, None]
+
+    sent_file = open(sys.argv[1])
+    tweet_file = open(sys.argv[2])
+
+    scores = {}
+    #for line in sent_file:
+        #term, score = line.split("\t")
+        #scores[term] = int(score)
+
+    for line in tweet_file:
+        tweet = json.loads(line)
+        word1, word2 = get_location_words(tweet)
+        if word1:
+            print word1, word2
+
+if __name__ == '__main__':
+    main()
