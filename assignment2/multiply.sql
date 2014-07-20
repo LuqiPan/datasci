ab029bb345e782ab978e95e624573a37f54c99e7
Luqi Pan <luqi.pan@gmail.com>
2014-07-19 21:52:21 -0400

    Computed the full matrix instead of only one cell

--- assignment2/multiply.sql
+++ assignment2/multiply.sql
@@ -1,5 +1,7 @@
-SELECT sum(a.value * b.value)
-FROM a, b
-WHERE a.col_num = b.row_num
-      AND a.row_num = 2
-      AND b.col_num = 3;
+SELECT value FROM(
+  SELECT sum(a.value * b.value) as value, a.row_num, b.col_num
+  FROM a, b
+  WHERE a.col_num = b.row_num
+  GROUP BY a.row_num, b.col_num
+)
+WHERE row_num = 2 AND col_num = 3;
